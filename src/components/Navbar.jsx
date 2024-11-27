c414526d52d9a643f06ebe5f58c794f93b03a6ee
Nanjula Perera <mamalsha6408@gmail.com>
2024-11-27 23:24:57 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <header>
+            <nav>
+                <img src="src\assets\images\React-logo.png.png" alt="React logo"/>
+                <span>React Facts</span>
+            </nav>
+        </header>
+    )
+}
